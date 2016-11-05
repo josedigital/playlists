ff6c80aafff46565b8591762f95f5809084b6803
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-11-04 20:59:32 -0500

    remove wire from repo

--- wire/modules/Inputfield/InputfieldAsmSelect/InputfieldAsmSelect.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".InputfieldAsmSelect select[multiple=multiple]").each(function() {
-		var $t = $(this); 
-		var options = config[$t.attr('id')]; 
-		$t.asmSelect(options); 
-	}); 
-}); 
--- wire/modules/Inputfield/InputfieldImage/InputfieldImage.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$("a.InputfieldFileLink").fancybox();
-	$(".InputfieldImage .InputfieldFileList").live('AjaxUploadDone', function() {
-		$("a.InputfieldFileLink", $(this)).fancybox(); 
-	}); 
-});
